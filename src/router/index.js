a057a7a243b4a64f422cac4727589a21bf38ca2f
Shinsou93 <evilshin78@yahoo.fr>
2022-05-11 16:14:45 +0200

    Début de la page Accueil 110522

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Accueil from '../views/Accueil.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Accueil',
+    component: Accueil
   },
   {
     path: '/about',
---
+++ src/views/Accueil.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <navbarOff />
+            <img class="Headerimg" src="../assets/Khmer/fondAccueil.jpg" alt="bgaccueil">
+            <h1 class="Titre">Bienvenu sur Ittadakimasu !</h1>
+    </div>
+</template>
+
+<script>
+import navbarOff from "../components/NavbarOff.vue"
+export default {
+  name: 'Accueil',
+  components: {
+      navbarOff
+  }
+}
+
+</script>
+
+<style scoped>
+#Accueil {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.Headerimg{
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    z-index: -1
+;
+}
+
+.Titre {
+    position: relative;
+}
+</style>
